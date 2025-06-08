8110873d71391def694cd4cd6218fdab640305f0
ridwanspace <ridwanspace.dotcom@gmail.com>
2025-06-09 01:19:05 +0700

    ♻️ refactor(docs): restyle documentation layout and theme
    - remove unused components and css
    - add new logo and update footer
    - set default theme to dark

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Layout } from 'nextra-theme-docs'
 import { Banner } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
@@ -9,23 +9,13 @@ export const metadata = {
 }
  
 const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
-const navbar = (
-  <Navbar
-    logo={<b>Nextra</b>}
-    // ... Your additional navbar options
-  />
-)
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
  
 export default async function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Layout
         banner={banner}
-        navbar={navbar}
         pageMap={await getPageMap()}
-        docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
-        footer={footer}
         // ... Your additional layout options
       >
         {children}
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
 
 const config = {
-  logo: <span>My Nextra Documentation</span>,
-  project: {
-    link: 'https://github.com/your-username/your-repo',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – FastAPI Best Practices',
+    }
   },
-  docsRepositoryBase: 'https://github.com/your-username/your-repo/tree/main',
+  logo: (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-6 w-6 text-sky-400"
+      >
+        <path d="M12 2L2 7l10 5 10-5-10-5z" />
+        <path d="M2 17l10 5 10-5" />
+        <path d="M2 12l10 5 10-5" />
+      </svg>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        FastAPI Guide
+      </span>
+    </>
+  ),
+  project: {
+    link: 'https://github.com/ridwanfathin/restful-fastapi-guideline',
+  },
+  docsRepositoryBase: 'https://github.com/ridwanfathin/restful-fastapi-guideline/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://fastapi.guide" target="_blank">
+          FastAPI Best Practices
+        </a>
+        .
+      </span>
+    )
   },
   codeHighlight: true,
+  defaultTheme: 'dark',
 }
 
 export default config
