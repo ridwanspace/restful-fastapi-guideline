07c27672e5bdfd2b7c0ceefef554d2eed574cb7c
ridwanspace <ridwanspace.dotcom@gmail.com>
2025-06-10 14:46:59 +0700

    ✨ feat(docs): enhance documentation layout with custom navbar and footer
    - add custom navbar with logo and title for improved branding
    - include footer with copyright information and license details

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,18 +1,44 @@
-import { Layout } from 'nextra-theme-docs'
+import { Layout, Navbar, Footer } from 'nextra-theme-docs'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
- 
+
+
 export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
  
 // const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
- 
+const navbar = (
+  <Navbar
+    logo={<>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-6 w-6 text-sky-400"
+      >
+        <path d="M12 2L2 7l10 5 10-5-10-5z" />
+        <path d="M2 17l10 5 10-5" />
+        <path d="M2 12l10 5 10-5" />
+      </svg>
+      <span className="text-xl font-bold">RESTful FastAPI Guidelines</span>
+    </>}
+    // ... Your additional navbar options
+  />
+)
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer> 
 export default async function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Layout
+        navbar={navbar}
         pageMap={await getPageMap()}
         docsRepositoryBase="https://github.com/ridwanspace/restful-fastapi-guideline/tree/main"
         editLink={'Edit this page on GitHub'}
